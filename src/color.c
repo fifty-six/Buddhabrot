a5d3810cdfabb30efabcf398e1d45d0d97bcf431
5FiftySix6 <ybham6@gmail.com>
2020-11-30 04:49:34 -0500

    Initial

---
+++ src/color.c
@@ -0,0 +1,240 @@
+//
+// Yusuf Bham, 29 November 2020
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+
+#define MAX_X 1440
+#define MAX_Y 1080
+
+#define MAX_ITER1 1000
+#define MAX_ITER2 500
+#define MAX_ITER3 5000
+
+#define MIN_ITER 150
+
+#define POINT_MAX 1e9
+
+#define MAX_MAG 10
+
+double lerp(double v0, double v1, double t) {
+  return (1 - t) * v0 + t * v1;
+}
+
+struct Color
+{
+    int r;
+    int g;
+    int b;
+};
+
+struct Path
+{
+    double re[MAX_ITER3];
+    double im[MAX_ITER3];
+};
+
+bool mandelbrot(int max_iter, double re_c, double im_c, struct Path *path, int *n)
+{
+    double a = 0;
+    double b = 0;
+
+    int i = 0;
+
+    for (;i < max_iter; i++)
+    {
+        double mag = a*a + b*b;
+
+        if (mag >= MAX_MAG)
+        {
+            *n = i;
+
+            return i > MIN_ITER;
+        }
+
+        double a_new = a*a - b*b + re_c;
+
+        b = 2*a*b + im_c;
+
+        a = a_new;
+
+        path -> re[i] = a;
+        path -> im[i] = b;
+    }
+
+    return false;
+}
+
+void write_file(struct Color** grid, char* fname, size_t max_y, size_t max_x)
+{
+    FILE *fout = fopen(fname, "w");
+
+    fprintf(fout, "P3\n");
+    fprintf(fout, "%d %d\n", (int) max_x, (int) max_y);
+    fprintf(fout, "255\n");
+
+    for (size_t y = 0; y < max_y; y++ )
+    {
+        for (size_t x = 0; x < max_x; x++)
+        {
+            struct Color c = grid[y][x];
+
+            fprintf(fout, "%d %d %d\n", c.r, c.g, c.b);
+        }
+    }
+
+    fclose(fout);
+}
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int main(void)
+{
+    srand(time(NULL));
+
+    struct Color** cgrid = malloc(sizeof(struct Color*) * MAX_Y);
+
+    for (size_t i = 0; i < MAX_Y; i++)
+        cgrid[i] = malloc(sizeof(struct Color) * MAX_X);
+
+    struct Path *path = malloc(sizeof(struct Path));
+
+    int (*rgrid)[MAX_X] = malloc(sizeof(int) * MAX_X * MAX_Y);
+    int (*ggrid)[MAX_X] = malloc(sizeof(int) * MAX_X * MAX_Y);
+    int (*bgrid)[MAX_X] = malloc(sizeof(int) * MAX_X * MAX_Y);
+
+    int *rline = (int*) rgrid;
+    int *gline = (int*) ggrid;
+    int *bline = (int*) bgrid;
+
+    memset(rline, 0, sizeof(int) * MAX_X * MAX_Y);
+    memset(gline, 0, sizeof(int) * MAX_X * MAX_Y);
+    memset(bline, 0, sizeof(int) * MAX_X * MAX_Y);
+
+    printf("Starting.\n");
+
+    const int min_dim = min(MAX_X, MAX_Y);
+
+    #pragma omp parallel for
+    for (int g = 0; g < 3; g++) 
+    {
+        for (int i = 0; i < POINT_MAX; i++)
+        {
+            int n;
+
+            double re_0 = 6 * drand48() - 3;
+            double im_0 = 6 * drand48() - 3;
+
+            double cr = re_0 * re_0 + im_0 * im_0;
+            double ci = sqrt(cr - re_0/2 + 0.0625);
+
+            int max_iter = 0;
+
+            switch (g) 
+            {
+                case 0:
+                    max_iter = MAX_ITER1;
+                    break;
+                case 1:
+                    max_iter = MAX_ITER2;
+                    break;
+                case 2:
+                    max_iter = MAX_ITER3;
+                    break;
+            }
+
+            if ((16 * cr * ci) < (5 * ci - 4 * re_0 + 1))
+                continue;
+
+            if (!mandelbrot(max_iter, re_0, im_0, path, &n))
+                continue;
+
+            for (int j = 0; j < n; j++) 
+            {
+                int ix = 0.3 * MAX_X * path -> re[j] + MAX_X / 2;
+                int iy = 0.3 * MAX_X * path -> im[j] + MAX_Y / 2;
+
+                if (iy >= MAX_Y || ix >= MAX_X || ix < 0 || iy < 0)
+                    continue;
+
+                switch (g)
+                {
+                    case 0:
+                        bgrid[iy][ix]++;
+                        break;
+                    case 1:
+                        ggrid[iy][ix]++;
+                        break;
+                    case 2:
+                        rgrid[iy][ix]++;
+                        break;
+                }
+            }
+        }
+    }
+
+    int rmaximum = INT_MIN;
+    int gmaximum = INT_MIN;
+    int bmaximum = INT_MIN;
+
+    for (size_t i = 0; i < MAX_X * MAX_Y; i++)
+    {
+        rmaximum = max(rmaximum, rline[i]);
+        gmaximum = max(gmaximum, gline[i]);
+        bmaximum = max(bmaximum, bline[i]);
+    }
+
+    for (size_t y = 0; y < MAX_Y; y++)
+    {
+        for (size_t x = 0; x < MAX_X; x++)
+        {
+            double rvalue = pow(rgrid[y][x] * 1.0 / rmaximum, 1.0 / 3);
+            double gvalue = pow(ggrid[y][x] * 1.0 / gmaximum, 1.0 / 3);
+            double bvalue = pow(bgrid[y][x] * 1.0 / bmaximum, 1.0 / 3);
+
+            if (rvalue > 1) rvalue = 1;
+            if (gvalue > 1) gvalue = 1;
+            if (bvalue > 1) bvalue = 1;
+
+            cgrid[y][x] = (struct Color) {
+                    .r = rvalue * 255,
+                    .g = gvalue * 255,
+                    .b = bvalue * 255
+            };
+        }
+    }
+
+    printf("Finished frame.\n");
+
+    write_file(cgrid, "out_color.ppm", MAX_Y, MAX_X);
+
+    printf("Wrote file.\n");
+
+    for (size_t i = 0; i < MAX_Y; i++)
+        free(cgrid[i]);
+
+    free(cgrid);
+    free(path);
+
+    free(rgrid);
+    free(ggrid);
+    free(bgrid);
+
+    return 0;
+}
+//
+// end of file
+//
